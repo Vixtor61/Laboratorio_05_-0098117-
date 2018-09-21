e5e2933bbdb12e264710e0f89ef34cad2b3c216e
Vixtor61 <00198117@uca.edu.sv>
2018-09-21 09:26:10 -0600

    aksd

--- ejercicio5.js
+++ ejercicio5.js
@@ -1,8 +1,11 @@
 conver(decimal);
 r = prompt("ingrese el radio para calcular el area");
 var cir  = (r)  =>{
+    if(r<=0){
+        return -1;
+    }
     var a = 3.141516*(r*r);
-    console.log("el area es =" + a);
+    return a;
 
 }
-cir(r);
+console.log(cir(r));
